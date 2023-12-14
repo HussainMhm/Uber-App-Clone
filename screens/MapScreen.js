5dbd7ad9502239343507056ad7fe15ac8c5af58b
hussain <hussainabdullatif@marun.edu.tr>
2023-12-15 02:54:04 +0300

    Add back button navigation

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Map from "../components/Map";
 import NavigateCard from "../components/NavigateCard";
 import RideOptionsCard from "../components/RideOptionsCard";
+import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
 const MapScreen = () => {
+    const navigation = useNavigation();
+
     return (
         <View>
+            <TouchableOpacity
+                className="bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg"
+                onPress={() => navigation.navigate("Home")}
+            >
+                <Icon name="menu" />
+            </TouchableOpacity>
+
             <View className="h-1/2">
                 <Map />
             </View>
